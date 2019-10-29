5fa33cf01fce539d15b04a68246e45abaa2b7eb5
Daniel Brown <djb16b@my.fsu.edu>
2019-10-28 21:17:52 -0400

    Started on map loading
    Added a barebones map loader, still need to add most features to it
    Note: doesnt run yet, still need to change a lot in how gamescreen.py
    calls maps.py

--- src/maps.py
+++ src/maps.py
@@ -1,47 +1,94 @@
 import pygame
+from enum import Enum
+
+class MapLoadState(enum):
+    MAPSIZE = 0
+    MAPNAME = 1
+    MAPINFO = 2
+    ZONEINFO = 3
+    ENEMY = 4
+    COLLIDABLE = 5
 
 
 class Map:
 
-    def __init__(self, screen, game_settings, background_path, name='Map'):
-        # Maps can store the screen and background image for all maps
-
-        self.name = name
+    def __init__(self, screen, game_settings):
+        #simple init to get needed objects and initialize a few map variables
         self.screen = screen
+        self.game_settings = game_settings
         self.screen_rect = self.screen.get_rect()
+        self.sizex = self.sizey = 0
+        self.name = ""
+        self.spawnpoint = (0,0)
 
-        # Load, scale, and position background image
-        self.bg = pygame.image.load(background_path)
-        self.bg = pygame.transform.scale(
-            self.bg, (game_settings.screen_w, game_settings.screen_h))
+        #function to load level
+        #is given path by gamescreen
+        def loadMap(self, level_path):
+            #to hold which state of maploading we are in
+            load_stage = MapLoadState(0)
 
-        self.bg_rect = self.bg.get_rect()
-        self.bg_rect.centerx = self.screen_rect.centerx
-        self.bg_rect.centery = self.screen_rect.centery
+            #to hold which zone we are currently working on
+            cur_zone = (0,0)
 
-        self.enemies = []
+            with open(level_path) as f:
+                line = f.readline()
 
-    def add_enemy(self, enemy):
-        self.enemies.append(enemy)
+                #check if the line isnt a comment, which is denoted by a #
+                if line[0] != '#':
+                    #first state, get the size of the map
+                    if(load_stage == MapLoadStage.MAPSIZE):
+                        #size is stored as x,y no spaces
+                        pos = line.find(',')
+                        self.sizex = line[:pos]
+                        self.sizey = line[pos+1:]
+                        #create 2d array of zones with size that we just read in
+                        self.zones = [[Zone() for j in range(self.sizex)] for i in range(self.sizey)]
+                        #set the next state to get map name
+                        load_stage = MapLoadStage.MAPNAME
 
-    def remove_enemy(self, enemy):
-        self.enemies.remove(enemy)
+                    #get map name
+                    elif(load_stage == MapLoadStage.MAPNAME):
+                        self.name = line
+                        #set the next state to be general map info
+                        load_stage = MapLoadStage.MAPINFO
 
-    def blitme(self):
+                    #map loading state for general info
+                    #for now this state is just for going into the ZONEINFO state
+                    #also setting spawn
+                    #may add more later
+                    elif(load_stage == MapLoadStage.MAPINFO):
+                        #get position of equal sign
+                        #if its in cur line, check for keywords that need that
+                        #if not, check for keywords without it
+                        eqpos = line.find("=")
+                        if(eqpos == -1):
+                            #placeholder for if i need keywords in mapinfo state without a =
+                            pass
 
-        self.screen.blit(self.bg, self.bg_rect)
+                        else:
+                            if(line[:eqpos] == "editzone"):
+                                #editzone is keyword for editing new zone
+                                #after the equal sign should be the coords of the zone, as x,y
+                                pos = line.find(',')
+                                #set new current zone
+                                cur_zone[0] = line[eqpos+1:pos]
+                                cur_zone[1] = line[pos+1:]
+                                #set map loading state to zoneinfo
+                                load_state = MapLoadingState.ZONEINFO
 
-        for enemy in self.enemies:
-            enemy.blitme()
+                            elif(line[:eqpos] == "setspawn"):
+                                #set spawn is keyword for setting initial player spawn to certain zone
+                                #after the equal sign should be the coords of the zone, as x,y
+                                pos = line.find(',')
+                                #set new current zone
+                                self.spawnpoint[0] = line[eqpos+1:pos]
+                                self.spawnpoint[1] = line[pos+1:]
+
+                    elif(load_stage == MapLoadStage.ZONEINFO):
+                        pass
 
 
-'''
-# Fill out class when class by class functionality arises
 
-class CityStreet:
-
-    def __init__(self, screen, game_settings, background_path):
-        super().__init__(screen, game_settings, background_path)
-
-        self.name = "City Street"
-'''
+class Zone:
+    def __init__(self, screen, game_settings):
+        pass
