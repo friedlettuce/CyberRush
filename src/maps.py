a154abd98fd2c6fe61e32991321c5022142d24cf
ga17 <ga17@my.fsu.edu>
2019-11-14 21:04:17 -0500

    Refactored Map

--- src/maps.py
+++ src/maps.py
@@ -35,8 +35,21 @@ class Map:
         self.cur_zone = [0, 0]
         self.line = None
 
-    #function to load level
-    #is given path by gamescreen
+        # Enemy attributes
+        self.enemy_x = None
+        self.enemy_y = None
+        self.enemy_endx = None
+        self.enemy_endy = None
+
+        # Collidable Attributes
+        self.col_x = None
+        self.col_y = None
+        self.col_w = None
+        self.col_h = None
+        self.col_color = None
+
+    # function to load level
+    # is given path by gamescreen
     def load_map(self, level_path):
 
         with open(level_path) as f:
@@ -46,7 +59,17 @@ class Map:
             if self.line[0] != '#':
 
                 if self.load_state == MapLoadState.MAPSIZE:
-
+                    self.map_size()
+                elif self.load_state == MapLoadState.MAPNAME:
+                    self.map_name()
+                elif self.load_state == MapLoadState.MAPINFO:
+                    self.map_info()
+                elif self.load_state == MapLoadState.ZONEINFO:
+                    self.zone_info()
+                elif self.load_state == MapLoadState.ENEMY:
+                    self.load_enemy()
+                elif self.load_state == MapLoadState.COLLIDABLE:
+                    self.load_collidable()
 
         # when done loading map, build connections
         self.build_connections()
@@ -89,14 +112,14 @@ class Map:
                 # after the equal sign should be the coords of the zone, as x,y
                 pos = self.line.find(',')
                 # set new current zone
-                cur_zone[0] = self.line[eqpos + 1:pos]
-                cur_zone[1] = self.line[pos + 1:]
+                self.cur_zone[0] = self.line[eqpos + 1:pos]
+                self.cur_zone[1] = self.line[pos + 1:]
                 # set used flag to True
-                self.zones[cur_zone[0]][cur_zone[1]].used = true
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].used = True
                 # set map loading state to zoneinfo
                 self.load_state = MapLoadState.ZONEINFO
 
-            elif (self.line[:eqpos] == "setspawn"):
+            elif self.line[:eqpos] == "setspawn":
                 # set spawn is keyword for setting initial player spawn to certain zone
                 # after the equal sign should be the coords of the zone, as x,y
                 pos = self.line.find(',')
@@ -110,52 +133,52 @@ class Map:
         # if its in cur self.line, check for keywords that need that
         # if not, check for keywords without it
         eqpos = self.line.find("=")
-        if (eqpos == -1):
-            if (self.line == "endzone"):
+        if eqpos == -1:
+            if self.line == "endzone":
                 # go back to mapinfo state
                 self.load_state = MapLoadState.MAPINFO
-            elif (self.line == "addenemy"):
+            elif self.line == "addenemy":
                 # go to add enemy state
                 self.load_state = MapLoadState.ENEMY
-            elif (self.line == "addcollidable"):
+            elif self.line == "addcollidable":
                 # go to add collidable state
                 self.load_state = MapLoadState.COLLIDABLE
 
         else:
-            if (self.line[:eqpos] == "setbg"):
+            if self.line[:eqpos] == "setbg":
                 # set background of zone
                 bgname = self.line[eqpos + 1:]
-                bgpath = os.path.join(self.gamesettings.resources_folder, bgname)
-                self.zones[curzone[0]][curzone[1]].set_bg(bgpath)
-            elif (self.line[:eqpos] == "setmusic"):
+                bgpath = os.path.join(self.game_settings.resources_folder, bgname)
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].set_bg(bgpath)
+            elif self.line[:eqpos] == "setmusic":
                 # set music of zone
                 musicname = self.line[eqpos + 1:]
                 musicpath = os.path.join(self.game_settings.music_folder, musicname)
-                self.zones[curzone[0]][curzone[1]].set_music(musicpath)
-            elif (self.line[:eqpos] == "setleftspawn"):
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].set_music(musicpath)
+            elif self.line[:eqpos] == "setleftspawn":
                 # set spawn when coming from left
                 pos = self.line.find(',')
                 x = self.line[eqpos + 1:pos]
                 y = self.line[pos + 1:]
-                self.zones[curzone[0]][curzone[1]].set_left_spawn(x, y)
-            elif (self.line[:eqpos] == "setrightspawn"):
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].set_left_spawn(x, y)
+            elif self.line[:eqpos] == "setrightspawn":
                 # set spawn when coming from right
                 pos = self.line.find(',')
                 x = self.line[eqpos + 1:pos]
                 y = self.line[pos + 1:]
-                self.zones[curzone[0]][curzone[1]].set_up_spawn(x, y)
-            elif (self.line[:eqpos] == "setleftspawn"):
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].set_up_spawn(x, y)
+            elif self.line[:eqpos] == "setleftspawn":
                 # set spawn when coming from up
                 pos = self.line.find(',')
                 x = self.line[eqpos + 1:pos]
                 y = self.line[pos + 1:]
-                self.zones[curzone[0]][curzone[1]].set_up_spawn(x, y)
-            elif (self.line[:eqpos] == "setdownspawn"):
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].set_up_spawn(x, y)
+            elif self.line[:eqpos] == "setdownspawn":
                 # set spawn when coming from down
                 pos = self.line.find(',')
                 x = self.line[eqpos + 1:pos]
                 y = self.line[pos + 1:]
-                self.zones[curzone[0]][curzone[1]].set_down_spawn(x, y)
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].set_down_spawn(x, y)
 
     def load_enemy(self):
 
@@ -163,24 +186,24 @@ class Map:
         # if its in cur self.line, check for keywords that need that
         # if not, check for keywords without it
         eqpos = self.line.find("=")
-        if (eqpos == -1):
-            if (self.line == "endenemy"):
+        if eqpos == -1:
+            if self.line == "endenemy":
                 # create enemy object and add to enemies list for current zone
-                enemy = HoveringEnemy(self.screen, self.game_settings, self.enemyx, self.enemyy,
+                enemy = HoveringEnemy(self.screen, self.game_settings, self.enemy_x, self.enemy_y,
                                       self.game_settings.hov_size[0],
-                                      self.game_settings.hov_size[0], self.enemyendx, self.enemyendy)
+                                      self.game_settings.hov_size[0], self.enemy_endx, self.enemy_endy)
                 # go back to zoneinfo state
                 self.load_state = MapLoadState.ZONEINFO
 
         else:
-            if (self.line[:eqpos] == "setpos"):
+            if self.line[:eqpos] == "setpos":
                 pos = self.line.find(',')
-                self.enemyx = self.line[eqpos + 1:pos]
-                self.enemyy = self.line[pos + 1:]
-            elif (self.line[:eqpos] == "setendx"):
-                self.enemyendx = self.line[eqpos + 1:]
-            elif (self.line[:eqpos] == "setendy"):
-                self.enemyendy = self.line[eqpos + 1:]
+                self.enemy_x = self.line[eqpos + 1:pos]
+                self.enemy_y = self.line[pos + 1:]
+            elif self.line[:eqpos] == "setendx":
+                self.enemy_endx = self.line[eqpos + 1:]
+            elif self.line[:eqpos] == "setendy":
+                self.enemy_endy = self.line[eqpos + 1:]
 
     def load_collidable(self):
 
@@ -188,57 +211,59 @@ class Map:
         # if its in cur self.line, check for keywords that need that
         # if not, check for keywords without it
         eqpos = self.line.find("=")
-        if (eqpos == -1):
-            if (self.line == "endcollidable"):
+        if eqpos == -1:
+            if self.line == "endcollidable":
                 # create rect for collidable
-                r = Rect(self.colx, self.coly, self.colw, self.colh)
+                r = pygame.Rect(self.col_x, self.col_y, self.col_w, self.col_h)
                 # create color tuple
-                color = (self.colcolor[0:3], self.colcolor[3:6], self.colcolor[6:])
+                color = (self.col_color[0:3], self.col_color[3:6], self.col_color[6:])
                 # create collidable object and add to collidables list for current zone
                 c = Collidable(self.screen, r, color)
-                self.zones[curzone[0]][curzone[1]].add_collidable(c)
+                self.zones[self.cur_zone[0]][self.cur_zone[1]].add_collidable(c)
                 # go back to zoneinfo state
                 self.load_state = MapLoadState.ZONEINFO
 
         else:
-            if (self.line[:eqpos] == "setpos"):
+            if self.line[:eqpos] == "setpos":
                 pos = self.line.find(',')
-                self.colx = self.line[eqpos + 1:pos]
-                self.coly = self.line[pos + 1:]
-            elif (self.line[:eqpos] == "setdims"):
+                self.col_x = self.line[eqpos + 1:pos]
+                self.col_y = self.line[pos + 1:]
+            elif self.line[:eqpos] == "setdims":
                 pos = self.line.find(',')
-                self.colw = self.line[eqpos + 1:pos]
-                self.colh = self.line[pos + 1:]
-            elif (self.line[:eqpos] == "setcolor"):
-                self.colcolor = self.line[eqpos + 1:]
+                self.col_w = self.line[eqpos + 1:pos]
+                self.col_h = self.line[pos + 1:]
+            elif self.line[:eqpos] == "setcolor":
+                self.col_color = self.line[eqpos + 1:]
 
     def build_connections(self):
-        #this function builds the connections between the zones of the maps
-        #sets whether going to the edge of a zone should take the player
-        #to the next zone or not
-        for x,y in itertools.product(range(self.sizex), range(self.sizey)):
-            if(self.zones[x][y].used):
-                #only check zones which are used
-                if(y>0):
-                    #if y>0, check the zone above us, which is y-1
-                    if(self.zones[x][y-1]):
-                        #if this zone is used, set up zone used flag in current zone to true
-                        self.zones[x][y].up_used = true
-                if(y<self.sizey-1):
-                    #if y<max size, check the zone below us, which is y+1
-                    if(self.zones[x][y+1]):
-                        #if this zone is used, set up zone used flag in current zone to true
-                        self.zones[x][y].down_used = true
-                if(x>0):
-                    #if y>0, check the zone to our left, which is x-1
-                    if(self.zones[x-1][y]):
-                        #if this zone is used, set left zone used flag in current zone to true
-                        self.zones[x][y].left_used = true
-                if(x<self.sizex-1):
-                    #if x<max size, check the zone to our right, which is x+1
-                    if(self.zones[x+1][y]):
-                        #if this zone is used, set right zone used flag in current zone to true
-                        self.zones[x][y].right_used = true
+
+        # this function builds the connections between the zones of the maps
+        # sets whether going to the edge of a zone should take the player
+        # to the next zone or not
+
+        for x, y in itertools.product(range(self.sizex), range(self.sizey)):
+            if self.zones[x][y].used:
+                # only check zones which are used
+                if y > 0:
+                    # if y>0, check the zone above us, which is y-1
+                    if self.zones[x][y-1]:
+                        # if this zone is used, set up zone used flag in current zone to true
+                        self.zones[x][y].up_used = True
+                if y < self.sizey-1:
+                    # if y<max size, check the zone below us, which is y+1
+                    if self.zones[x][y+1]:
+                        # if this zone is used, set up zone used flag in current zone to true
+                        self.zones[x][y].down_used = True
+                if x > 0:
+                    # if y>0, check the zone to our left, which is x-1
+                    if self.zones[x-1][y]:
+                        # if this zone is used, set left zone used flag in current zone to true
+                        self.zones[x][y].left_used = True
+                if x < self.sizex-1:
+                    # if x<max size, check the zone to our right, which is x+1
+                    if self.zones[x+1][y]:
+                        # if this zone is used, set right zone used flag in current zone to true
+                        self.zones[x][y].right_used = True
 
 
 class Zone:
@@ -250,14 +275,16 @@ class Zone:
 
         self.game_settings = game_settings
         self.music = "none"
-        #variable to hold if this zone is used in the map
-        self.used = false
-        #variables to hold if the zones around this one are used
-        #used for transfering between zones
-        self.left_used = false
-        self.right_used = false
-        self.up_used = false
-        self.down_used = false
+
+        # variable to hold if this zone is used in the map
+        self.used = False
+
+        # variables to hold if the zones around this one are used
+        # used for transfering between zones
+        self.left_used = False
+        self.right_used = False
+        self.up_used = False
+        self.down_used = False
 
         self.bg = None
         self.bg_rect = self.bg.get_rect()
